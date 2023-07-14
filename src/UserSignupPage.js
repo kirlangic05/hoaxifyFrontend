2108900f3b5809aafd54a75f8c231122643de497
Furkan Melih <furkanmelihkirlangic@gmail.com>
2023-07-15 00:47:12 +0300

    s

---
+++ src/UserSignupPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class UserSignupPage extends React.Component{
+    render(){
+        return(
+            <h1>SignUp</h1>
+        );
+    }
+}
+export default UserSignupPage;
